06cb82d0834441686fccd0cc8958f60f5589c147
Mart McNamara <Martin.McNamara@gmail.com>
2019-12-21 22:44:03 +0000

    Only draw whats needs on the screen.

--- SFML1/main.cpp
+++ SFML1/main.cpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <string>
 #include <windows.h>
+#include "fps.hpp"
 #include "logline.h"
 
 // TODO
@@ -14,10 +15,10 @@
 // Column Support
 // Header Row
 // Scroll Bar
-// Only draw whats needs on the screen.
 // Stop scrolling when nothing on screen.
 // Why the console
 // Crash on exit
+// Lock needed?
 
 
 #define DEFAULT_WINDOW_WIDTH_PX		640
@@ -42,18 +43,21 @@ std::mutex mtx;
 
 class displayMngr {
 private:
-	uint32_t dispLogentryCount;
 	uint16_t screen_width_px;
 	uint16_t screen_height_px;
+	uint32_t screen_vertical_position;
 
 public:
 	displayMngr();
 	~displayMngr();
-	void addNewDisplayData();
-	bool checkDisplayUpdated();
+	void updateDataForView();
+
 	void setViewSize(uint16_t x, uint16_t y);
 	uint16_t getViewSizeX();
 	uint16_t getViewSizeY();
+	void updateViewVerticalPosition(int32_t);
+	uint32_t getViewVerticalPosition();
+	
 	void teleType();
 
 public:
@@ -62,15 +66,12 @@ public:
 	sf::Time timeTele;
 	std::vector<sf::Text> lineTextVec;
 	std::vector<_line_points> lineTextSepVec;
-	int32_t screen_view_y_postion;
-
 };
 
 
 displayMngr::displayMngr()
 {
-	dispLogentryCount = 0;
-	screen_view_y_postion = 0;
+	screen_vertical_position = 0;
 	screen_width_px = DEFAULT_WINDOW_WIDTH_PX;
 	screen_height_px = DEFAULT_WINDOW_HEIGHT_PX;
 
@@ -81,14 +82,25 @@ displayMngr::~displayMngr()
 {
 }
 
-void displayMngr::addNewDisplayData()
+void displayMngr::updateDataForView()
 {
+	uint32_t postion = 0;
+	uint32_t dispLogindex = 0;
 	auto plog = test_book->getlogBook();
-	timeTele = clockTele.getElapsedTime();
-	
 	lineTextVec.clear();
 	lineTextSepVec.clear();
-	for (dispLogentryCount = 0; dispLogentryCount < plog->size(); dispLogentryCount++)
+
+	postion = getViewVerticalPosition();
+
+	while ((dispLogindex * TEXT_CHAR_HEIGHT_PX) <= postion)
+	{
+		dispLogindex++;
+	}
+	dispLogindex--;
+	//std::cout << "ViewDataUpdate Starts at index " << dispLogindex << std::endl;
+
+
+	for (dispLogindex; dispLogindex < plog->size(); dispLogindex++)
 	{
 		sf::Text lineText;
 		_line_points lineTextSep;
@@ -97,36 +109,29 @@ void displayMngr::addNewDisplayData()
 		lineText.setFont(text_font);
 		lineText.setFillColor(sf::Color::Green);
 		lineText.setCharacterSize(TEXT_CHAR_HEIGHT_PX);
-		lineText.setPosition(0, (float)dispLogentryCount * TEXT_CHAR_HEIGHT_PX);
+		lineText.setPosition(0, (float)dispLogindex * TEXT_CHAR_HEIGHT_PX);
 
 		// Display string consist of the first char only
-		std::string logline_partial_text = plog->at(dispLogentryCount).logline_text;
-		uint16_t logline_partial_size = plog->at(dispLogentryCount).logline_len_dspy;
+		std::string logline_partial_text = plog->at(dispLogindex).logline_text;
+		uint16_t logline_partial_size = plog->at(dispLogindex).logline_len_dspy;
 		lineText.setString(logline_partial_text.substr(0, logline_partial_size));
 		lineTextVec.push_back(lineText);
 
-		lineTextSep.line[0].position = sf::Vector2f((float)0, (float)(((dispLogentryCount + 1) * TEXT_CHAR_HEIGHT_PX) + TEXT_LINE_SEPERATOR_PX));
+		lineTextSep.line[0].position = sf::Vector2f((float)0, (float)(((dispLogindex + 1) * TEXT_CHAR_HEIGHT_PX) + TEXT_LINE_SEPERATOR_PX));
 		lineTextSep.line[0].color = sf::Color(DARK_GREY);
-		lineTextSep.line[1].position = sf::Vector2f((float)screen_width_px, (float)(((dispLogentryCount + 1) * TEXT_CHAR_HEIGHT_PX) + TEXT_LINE_SEPERATOR_PX));
+		lineTextSep.line[1].position = sf::Vector2f((float)screen_width_px, (float)(((dispLogindex + 1) * TEXT_CHAR_HEIGHT_PX) + TEXT_LINE_SEPERATOR_PX));
 		lineTextSep.line[1].color = sf::Color(DARK_GREY);
 
 		lineTextSepVec.push_back(lineTextSep);
+
+		// Dont calculate display data is its not in view
+		if ((dispLogindex * TEXT_CHAR_HEIGHT_PX) > (getViewVerticalPosition() + getViewSizeY()))
+		{
+			//std::cout << "ViewDataUpdate Complete at index " << dispLogindex << std::endl;
+			break;
+		}
 	}
 
-}
-
-bool displayMngr::checkDisplayUpdated()
-{
-	auto plog = test_book->getlogBook();
-
-	if (dispLogentryCount < plog->size())
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
 }
 
 void displayMngr::setViewSize(uint16_t x, uint16_t y)
@@ -146,8 +151,31 @@ uint16_t displayMngr::getViewSizeY()
 	return screen_height_px;
 }
 
+void displayMngr::updateViewVerticalPosition(int32_t update_postion)
+{
 
-volatile uint32_t timestamp;
+	if(update_postion > 0)
+		screen_vertical_position += update_postion;
+	else
+	{
+		int32_t position;
+		position = screen_vertical_position;
+		position += update_postion;
+
+		if (position < 0)
+			screen_vertical_position = 0;
+		else
+			screen_vertical_position = position;
+	}
+}
+
+
+
+uint32_t displayMngr::getViewVerticalPosition()
+{
+	return screen_vertical_position;
+}
+
 void displayMngr::teleType()
 {
 	uint16_t logline_partial_size;
@@ -178,7 +206,7 @@ void displayMngr::teleType()
 			timestamp = plog->at(i).logline_timestamp + TELETYPE_SPEED_CHAR_MS;
 			//std::cout << "timestamp:" << timestamp << std::endl;
 
-			// TODO - potentially more than one char
+			// TODO - potentially more than one char should be updated
 			if(current_timestamp > timestamp)
 			{
 				//std::cout << "UP!" << std::endl;
@@ -186,7 +214,6 @@ void displayMngr::teleType()
 
 				plog->at(i).logline_timestamp = current_timestamp;
 			}
-
 
 		}
 	}
@@ -232,13 +259,14 @@ int main()
 	// Create the display Manager
 	displayMngr screenTextLines;
 
-	//std::this_thread::sleep_for(std::chrono::milliseconds(1336));
-
 	// Thread to generate some test data
 	std::thread worktask(test_data_thread, screenTextLines.test_book);
 
-	sf::Clock clock; // starts the clock
-	sf::Time el = clock.getElapsedTime();
+	// Starts the clock
+	sf::Clock clock;
+
+	//
+	FPS fps_info;
 
 	while (window.isOpen())
 	{
@@ -256,13 +284,14 @@ int main()
 
 			case sf::Event::Resized:
 			{
-				sf::FloatRect visable_area(0, (float)screenTextLines.screen_view_y_postion, (float)event.size.width, (float)event.size.height);
+				sf::FloatRect visable_area(0, (float)screenTextLines.getViewVerticalPosition(), (float)event.size.width, (float)event.size.height);
 				screenTextLines.setViewSize(event.size.width, event.size.height);
 				window.setView(sf::View(visable_area));
 				break;
 			}
 			case sf::Event::KeyReleased:
 			{
+				std::cout << fps_info.getFPS() << std::endl;
 				break;
 			}
 			case sf::Event::MouseWheelScrolled:
@@ -270,34 +299,29 @@ int main()
 
 				if (event.mouseWheelScroll.delta > 0)
 				{
-					screenTextLines.screen_view_y_postion -= MOUSE_WHEEL_MOVE_PX;
-
-					if (screenTextLines.screen_view_y_postion < 0)
-						screenTextLines.screen_view_y_postion = 0;
+					screenTextLines.updateViewVerticalPosition(-MOUSE_WHEEL_MOVE_PX);
 				}
 				else
-					screenTextLines.screen_view_y_postion += MOUSE_WHEEL_MOVE_PX;
+					screenTextLines.updateViewVerticalPosition(MOUSE_WHEEL_MOVE_PX);
 
-
-
-				sf::FloatRect visable_area(0, (float)screenTextLines.screen_view_y_postion,(float)screenTextLines.getViewSizeX(), (float)screenTextLines.getViewSizeY());
+				sf::FloatRect visable_area(0, (float)screenTextLines.getViewVerticalPosition(),(float)screenTextLines.getViewSizeX(), (float)screenTextLines.getViewSizeY());
 				window.setView(sf::View(visable_area));
-				//std::cout << screenTextLines.screen_view_y_postion << "\n";
+				std::cout << screenTextLines.getViewVerticalPosition() << "\n";
 				break;
 			}
 
 			}
 		}
 
-
 		// Update the text
 		screenTextLines.teleType();
 
-		// Draw the screen
-		screenTextLines.addNewDisplayData();
+		// Draw the view
+		screenTextLines.updateDataForView();
 
 		window.clear(sf::Color::Black);
 
+		// Is this lock needed??
 		mtx.lock();
 
 		// Draw Line Seperators
@@ -318,6 +342,7 @@ int main()
 
 		window.display();
 
+		fps_info.update();
 
 	}
 
